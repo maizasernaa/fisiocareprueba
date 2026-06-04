b43c80cc14ecf462b797ae8efcddda7a28adf6fe
christo54acev@gmail.com <christo54acev@gmail.com>
2026-06-03 23:38:59 -0500

    fix: corregida importacion default de Landing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Landing } from './pages/Landing';
+import Landing from './pages/Landing';
 import './index.css'; // Estilos globales
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
