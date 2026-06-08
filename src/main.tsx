4f83a4a58f40f15c3a51ab7a68d791703944d063
Ale1519 <aurbizagastegui@gmail.com>
2026-06-08 01:35:05 -0500

    Add route for PerfilPaciente component

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,7 @@ import DashboardPaciente from './pages/DashboardPaciente';
 import DashboardFisio from './pages/DashboardFisio'; 
 import EspecialistaDetalle from './pages/EspecialistaDetalle'; 
 import AgendarCita from './pages/AgendarCita'; 
+import PerfilPaciente from './pages/PerfilPaciente';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -32,6 +33,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/dashboard-fisio" element={<DashboardFisio />} /> {/* 2. Registramos la ruta */}
         <Route path="/especialistas/:id" element={<EspecialistaDetalle />} />
         <Route path="/agendar/:id" element={<AgendarCita />} />
+        <Route path="/perfil-paciente" element={<PerfilPaciente />} />
       </Routes>
     </Router>
   </React.StrictMode>,
